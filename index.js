bba9ec1d74c63abdefb8d9d7d0f0f06f4576daec
Pablo Garcia <pablo.garcia@itserv.com.br>
2016-12-02 15:19:12 -0200

    atualização do cors

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
-    res.header("Acess-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'X-Request-With, content-type, Authorization');
+  next();
 });
 
 var sql = require('mssql');
